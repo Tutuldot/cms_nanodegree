3ba180641855b763416e6e25b4d08192376fbf9a
Anthony Estrada <azestrada@outlook.com>
2025-03-11 15:48:46 +0800

    Update __init__.py trying to fix loggin

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # loggin
 
+wsgi_app = app.wsgi_app
 app.logger.setLevel(logging.WARNING)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
