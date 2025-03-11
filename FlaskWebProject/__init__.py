e7c0b284f569ffa3f886dc6bdfde5d1ff0b7b99c
Anthony Estrada <anthony.estrada@chevron.com>
2025-03-11 16:00:33 +0800

    update loggin

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,7 +1,5 @@
-"""
-The flask application package.
-"""
 import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -10,14 +8,35 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# loggin
+
+# Set up logging configuration
+if not app.debug:
+    # Create a file handler object
+    file_handler = RotatingFileHandler('flask_app.log', maxBytes=10240, backupCount=10)
+    
+    # Set the logging level
+    file_handler.setLevel(logging.INFO)
+    
+    # Create a formatter object
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    
+    # Set the formatter for the file handler
+    file_handler.setFormatter(formatter)
+    
+    # Add the file handler to the app's logger
+    app.logger.addHandler(file_handler)
+
+    # Set the logging level for the app's logger
+    app.logger.setLevel(logging.INFO)
+
+    # Log that the application has started
+    app.logger.info('Flask application startup')
 
 wsgi_app = app.wsgi_app
-app.logger.setLevel(logging.WARNING)
+app.logger.setLevel(logging.DEBUG)
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.WARNING)
+streamHandler.setLevel(logging.DEBUG)
 app.logger.addHandler(streamHandler)
-
 
 Session(app)
 db = SQLAlchemy(app)
