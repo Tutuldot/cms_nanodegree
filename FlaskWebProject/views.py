cd6d787f5bc8331baa163db27cde96e60bb38690
Anthony Estrada <azestrada@outlook.com>
2025-03-12 09:04:55 +0800

    Update views.py fix logs

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -19,7 +19,7 @@ imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.n
 @app.route('/home')
 @login_required
 def home():
-    app.logger.info('App ok. Home')
+    app.logger.info(f"Hope page loaded for user: {current_user.username}")
     user = User.query.filter_by(username=current_user.username).first_or_404()
     posts = Post.query.all()
     return render_template(
@@ -68,13 +68,16 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            # error
             flash('Invalid username or password')
-            app.logger.error("Invalid login attempt.")
+            app.logger.info('%s Failed to login, invalid username or password', form.username.data)
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+            # success
+            app.logger.info('%s Logged in successfully', user.username)
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
